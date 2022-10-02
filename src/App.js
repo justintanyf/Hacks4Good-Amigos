1c0df41e9aab84490a95dffbc0df3477bdc90295
Justin Tan <tan_yan_feng@u.nus.edu>
2022-10-01 23:57:55 -0400

    Additional improvements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+    const [state , setState] = useState({
+        email : "",
+        password : ""
+    })
+
+    const handleChange = (e) => {
+        const {id , value} = e.target
+        setState(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+    }
+
+    const getPassword(email) {
+
+    }
+
+    const handleSubmitClick = (e) => {
+        e.preventDefault();
+        if(state.password === state.confirmPassword) {
+            sendDetailsToServer()
+        } else {
+            props.showError('Passwords do not match');
+        }
+    }
   return (
       <>
           <div className="container mx-auto px-4 h-full">
               <div className="flex content-center items-center justify-center h-full">
                   <div className="w-full lg:w-4/12 px-4">
                       <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-                          <div className="rounded-t mb-0 px-6 py-6">
-                              <div className="text-center mb-3">
-                                  <h6 className="text-blueGray-500 text-sm font-bold">
-                                      Sign in with
-                                  </h6>
-                              </div>
-                              <hr className="mt-6 border-b-1 border-blueGray-300" />
-                          </div>
                           <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                              <div className="text-blueGray-400 text-center mb-3 font-bold">
-                                  <small>Or sign in with credentials</small>
-                              </div>
                               <form>
                                   <div className="relative w-full mb-3">
                                       <label
@@ -31,6 +45,9 @@ function App() {
                                           type="email"
                                           className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                                           placeholder="Email"
+                                          id="email"
+                                          value={state.email}
+                                          onChange={handleChange}
                                       />
                                   </div>
 
@@ -42,9 +59,12 @@ function App() {
                                           Password
                                       </label>
                                       <input
+                                          id="password"
                                           type="password"
                                           className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                                           placeholder="Password"
+                                          value={state.password}
+                                          onChange={handleChange}
                                       />
                                   </div>
                                   <div>
@@ -62,8 +82,9 @@ function App() {
 
                                   <div className="text-center mt-6">
                                       <button
-                                          className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                                          type="button"
+                                          className="bg-blueGray-800 active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                                          type="submit"
+                                          // onClick={signIn()}
                                       >
                                           Sign In
                                       </button>
